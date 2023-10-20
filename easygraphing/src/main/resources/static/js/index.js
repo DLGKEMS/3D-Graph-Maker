8081b8871c68dcc69381e8e63e1ef3d243da2be9
hojunSeo <hoojoun@gmail.com>
2023-10-20 22:56:17 +0900

    feat: 그래프 디자인 변경 - 그래프 랜덤 색상 지정 - 테두리 생성

--- easygraphing/src/main/resources/static/js/index.js
+++ easygraphing/src/main/resources/static/js/index.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+
+const fontSize = 5;
 
 function init(){
     const renderer = new THREE.WebGLRenderer();
@@ -26,6 +29,7 @@ function init(){
     makeGraph(1,10, scene);
     makeGraph(2,5, scene);
     makeGraph(3,8, scene);
+
     // orbitcontrol을 사용하기 위해서는 애니메이션이 실행되어야 한다.(boiler)
     animate()
     function animate() {
@@ -36,12 +40,52 @@ function init(){
 }
 
 function makeGraph(index, value, scene){
-    const geometry = new THREE.BoxGeometry( 1, value, 1 );
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    cube.position.x=index;
-    cube.position.y=value/2;
+    // graph
+    var geom = new THREE.BoxBufferGeometry();
+    var material = new THREE.ShaderMaterial({
+        uniforms: {
+            thickness: {
+                value: 1
+            },
+            color: {
+                value: new THREE.Color()
+            }
+        },
+
+        vertexShader: vertexShader,
+        fragmentShader: fragmentShader,
+        extensions: {derivatives: true},
+    });
+
+    const cube = new THREE.Mesh( geom, material );
+    cube.material.uniforms.color.value.set(Math.random() * 0xffffff);
+    console.log(cube)
+    cube.scale.y = value;
+    cube.position.set(index*1.3, value/2, 0);
     scene.add( cube );
+
+    // text
+    const textGeometry = new TextGeometry( 'Hello three.js!', {
+        size: 80,
+        height: 5,
+        curveSegments: 12,
+        bevelEnabled: true,
+        bevelThickness: 10,
+        bevelSize: 8,
+        bevelOffset: 0,
+        bevelSegments: 5
+    } );
+    const textMaterial = new THREE.MeshBasicMaterial( { color: 0xb2b2b2 } );
+    const text = new THREE.Mesh( textGeometry, textMaterial );
+
+
+    text.position.z = 0;
+
+    text.rotation.x = 0;
+    text.rotation.y = Math.PI * 2;
+
+    scene.add(text);
+
 }
 init()
 
