9c667d90728305f70a31eef2f4d2b113be02c87b
hojunSeo <hoojoun@gmail.com>
2023-11-21 22:57:07 +0900

    feat: 업로드 페이지 추가

---
+++ easygraphing/src/main/resources/static/js/sqlTest.js
@@ -0,0 +1,4 @@
+document.querySelector('#form-upload-file').addEventListener('submit', function(event){
+    document.querySelector('#custom-spinner').style.visibility='visible';
+    document.querySelector('#upload-cover').style.visibility='visible';
+});
